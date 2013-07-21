bb7d69ba99dca1df38e27094c5234e595326f6af
Gary Burge <garyburge@garyburge.com>
2013-07-21 09:57:55 -0400

    Remove require of Composer 'autoload.php' file in index.php, since it does not work with Yii's homegrown autoloader and probably never will

--- backend/www/index.php
+++ backend/www/index.php
@@ -9,8 +9,5 @@ defined('YII_DEBUG') or define('YII_DEBUG',true);
 // specify how many levels of call stack should be shown in each log message
 defined('YII_TRACE_LEVEL') or define('YII_TRACE_LEVEL',3);
 
-// register composer autoloader
-require_once(dirname(__FILE__).'/../../vendor/autoload.php');
-
 require_once($yii);
 Yii::createWebApplication($config)->run();
